e6c061806c3e11e216885be18e8567ce381e3cee
Andrey Evsikov <evs.andrey@gmail.com>
2016-10-29 09:54:18 +0000

    dev_commit

--- app/assets/javascripts/versions.js
+++ app/assets/javascripts/versions.js
@@ -9,7 +9,12 @@ $(document).on('turbolinks:load', function() {
 });
 
 function updateReportIcon(msg) {
-  console.log(msg)
+  console.log(msg);
+  var report_id= msg['id']['$oid'];
+  var suite_id = msg['suite']['$oid'];
+  var build_id = msg['build']['$oid'];
+  var cell = findByCoord("build-"+build_id,"suite-"+suite_id,"big-mama")
+  
   $("#cell-"+msg['id']['$oid']).empty();
   $(msg['message']).appendTo($("#cell-"+msg['id']['$oid']));
   $("#cell-"+msg['id']['$oid']).find( "span" ).on("click", function(e){ 
@@ -17,3 +22,11 @@ function updateReportIcon(msg) {
       LoadSliderWith("/reports/"+this.id,{});
   })
 }
+
+
+function findByCoord(colId,rowId,tableId) {
+  var table = $('$'+tableId);
+  var colIndex = table.find("th#"+colId).cellIndex;
+  var rowIndex = table.find("tr#"+rowId).rowIndex;
+  return table.find('tr')[rowIndex].find('td')[colIndex]
+};

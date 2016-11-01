8001c5f6f20b22c8e9a1ebe4553e85680e5102c6
Andrey Evsikov <evs.andrey@gmail.com>
2016-11-01 08:16:44 +0000

    dev_commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,18 @@ class Product
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Slug
+  include Mongoid::Paperclip
   
   field :name, type: String
   slug  :name
   
   field :description, type: String
+  
   has_many :versions, inverse_of: :product, dependent: :destroy
+  
+  has_mongoid_attached_file :logo
+  validates_attachment_content_type :logo, 
+                                    :content_type => /^image\/(png|gif|jpeg)/,
+                                    :message => 'only (png/gif/jpeg) images'
+
 end

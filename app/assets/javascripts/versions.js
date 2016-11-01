f5cd34739b22842c55345b38858ab3b23b22b458
Andrey Evsikov <evs.andrey@gmail.com>
2016-11-01 14:58:33 +0000

    dev_commit

--- app/assets/javascripts/versions.js
+++ app/assets/javascripts/versions.js
@@ -6,9 +6,36 @@ $(document).on('turbolinks:load', function() {
       e.preventDefault(); 
       LoadSliderWith("/reports/"+this.id,{});
     });
-    console.log("Stacked bar started");
+        console.log("Stacked bar started");
           var stackedbar = new D3StackedBar({ 
-            container: "#stackedbar"
+            container: "#stackedbar",
+            margin: {top: 20, left: 50, bottom: 50, right: 20},
+            spacing: 0.5,
+            dataUrl: null,
+            data: [ 
+                    { key: "category 1", values: [ { x: "FY2008", y: 10 }, { x: "FY2009", y: 20 }, { x: "FY2010", y: 30 }, { x: "FY2011", y: 5 }, { x: "FY2012", y: 15 } ]}, 
+                    { key: "category 2", values: [ { x: "FY2008", y: 20 }, { x: "FY2009", y: -40 }, { x: "FY2010", y: 20 }, { x: "FY2011", y: 5 }, { x: "FY2012", y: 10 } ]}, 
+                    { key: "category 3", values: [ { x: "FY2008", y: -10 }, { x: "FY2009", y: 10 }, { x: "FY2010", y: 10 }, { x: "FY2011", y: -10 }, { x: "FY2012", y: 10 } ]}, 
+                  ],
+            resizable: true,
+            showLegend: true,
+            showTooltip: true,
+            showRuler: true,
+            showHorizontalGrid: true,
+            displayTable: true,
+            yFormat: function(d) { return d; },
+            colors: ['rgb(166,206,227)','rgb(31,120,180)','rgb(178,223,138)','rgb(51,160,44)','rgb(251,154,153)','rgb(227,26,28)','rgb(253,191,111)','rgb(255,127,0)','rgb(202,178,214)','rgb(106,61,154)','rgb(255,255,153)','rgb(177,89,40)'],
+            xTickSize: function(base) { return 10; },
+            yTickSize: function(base) { return 10; },
+            xTicks: 5,
+            yTicks: 5,
+            xTickPadding: 5,
+            yTickPadding: 5,
+            yAxisTranslate: function(base) { return "translate(0,0)"; },
+            xAxisTranslate: function(base) { return "translate(0,"+base.height+")"; },
+            xTickFormat: function(d) { return d; },
+            yTickFormat: function(d) { return d; },
+            tooltipText: function(d, element) { return "<p>Tooltip<br />x: "+d.x+"<br />y:"+d.y+"<p>"; }
           });
           stackedbar.show();
 });

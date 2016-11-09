bdf392d56643a4b774a6fe3caec3026333c0eb59
Andrey Evsikov <evs.andrey@gmail.com>
2016-11-09 14:26:55 +0000

    dev_commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -102,7 +102,9 @@ class ReportsController < ApplicationController
     
     @report.result  = (params["result"] == 'passed')
     @report.error = params["error"]
-    @report.screenshot = params["screenshot"]
+    screen_file = Paperclip.io_adapters.for(params["screenshot"])
+    screen_file.original_filename = @report._id+"."+file['mime_type'].split("/")[1]
+    @report.screenshot = sreen_file
     @report.custom_params = params["custom_params"].to_json
     @report.save
     params["attachments"].each do |k,file|

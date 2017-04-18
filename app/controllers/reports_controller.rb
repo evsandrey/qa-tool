09dea379d5cc09b76df4e048bb0584351b9a6de4
Andrey Evsikov <evs.andrey@gmail.com>
2017-04-18 08:49:21 +0000

    dev_commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -88,8 +88,6 @@ class ReportsController < ApplicationController
 
   def report_end
     @report =  Report.new()
-    p "PARAMS"
-    p params["product"]
     product = Product.find_by(name: params["product"])
     version = Version.find_by(name: params["version"])
     

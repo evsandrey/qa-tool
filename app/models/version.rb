b90088bdab6fb1c0e9bece46ad8f907490410897
Andrey Evsikov <evs.andrey@gmail.com>
2017-04-20 09:07:28 +0000

    dev_commit

--- app/models/version.rb
+++ app/models/version.rb
@@ -14,6 +14,7 @@ class Version
   has_many :test_cases, dependent: :destroy
   has_many :categories, dependent: :destroy
   has_many :hosts, dependent: :destroy
+  has_many :reports, through: :test_cases
   
   after_create :create_defaults
   
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
       resources :builds
       resources :hosts
       resources :categories
+      resources :reports
       resources :test_cases do 
         resources :reports
       end

de5262db6b3082fa7516965cb77ee1a354093984
Andrey Evsikov <evs.andrey@gmail.com>
2017-04-24 09:35:15 +0000

    dev_commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -65,7 +65,7 @@ class ReportsController < ApplicationController
   end
   
   def mass_investigation_form 
-    @report_ids = params
+    @report_ids = params.to_s
     render :partial => '/reports/mass_investigation_form'
   end
   

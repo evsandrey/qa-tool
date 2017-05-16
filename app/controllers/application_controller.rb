15e6f9894fe1e60db5adfc63f4da5b5ec2cf9205
Andrey Evsikov <evs.andrey@gmail.com>
2017-05-16 08:39:00 +0000

    dev_commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_filter :chat_init, :set_timezone
-  prepend_before_filter :get_auth_token
+  # prepend_before_filter :get_auth_token
   
   # before_action :authenticate_user!
   
@@ -60,10 +60,10 @@ class ApplicationController < ActionController::Base
   end
   
   private
-    def get_auth_token
-    if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
-      params[:auth_token] = auth_token
-    end
+    # def get_auth_token
+    # if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
+    #   params[:auth_token] = auth_token
+    # end
   
   
 end
